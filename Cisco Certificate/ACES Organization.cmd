4f434e59ebd54a05516b585ecaefb55ba905be8a
MAYKAMAYKA <161400551+MAYKAMAYKA@users.noreply.github.com>
2026-01-27 17:36:00 +0800

    Update ACES Organization.cmd

--- Cisco Certificate/ACES Organization.cmd
+++ Cisco Certificate/ACES Organization.cmd
@@ -1,1 +1,15 @@
+# ACES Officer
 
+The Association of Computer Engineering Students (ACES) is the professional organization representing the Computer Engineering students of De La Salle Lipa. I was an officer for three years.
+
+## Executive President · Jun 2024 - Jun 2025Executive President · Jun 2024 - Jun 2025
+
+As Executive President of ACES, I lead the organization in advancing its mission to empower Computer Engineering students through impactful programs, strategic partnerships, and inclusive leadership. I oversee all executive functions, guide long-term planning, and ensure the successful implementation of academic, technical, and community-based initiatives. My role involves representing the organization in institutional collaborations, supporting committee heads, and fostering a cohesive environment where innovation, service, and student development thrive.
+
+## Vice President for Internal Affairs · Jun 2023 - Jun 2024
+
+As Vice President for Internal Affairs, I led the coordination of internal operations and member engagement within ACES. I oversaw the planning and execution of organizational activities, ensuring alignment with our core objectives while fostering collaboration among departments. My responsibilities included managing internal communications, facilitating general assemblies, and strengthening team dynamics to promote a culture of accountability, inclusivity, and active participation across all initiatives.
+
+## Ast. Finance Manger · Jun 2022 - Jun 2023Ast. Finance Manger · Jun 2022 - Jun 2023
+
+As Assistant Finance Manager of ACES (the professional school organization of Computer Engineering Students), I supported the strategic planning and execution of financial activities, ensuring transparent fund management and accurate reporting. I collaborated closely with the executive board to allocate resources for key student-led initiatives, balancing organizational needs with financial accountability. My role also involved streamlining budget processes, assisting in event funding proposals, and upholding financial compliance throughout the academic year.
